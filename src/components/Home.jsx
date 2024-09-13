f7c4fbf65279ed5a13f4e041fb4130862f19c003
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2024-09-13 11:55:28 +0530

    applied BrowserRouter in main.jsx and made some components

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Cards from './Cards';
+import Navbar from './Navbar.jsx';
+export default function Home(){
+    return (
+        <>
+            <Navbar />
+            <div className="bg-red-300 w-[82%] h-[100%] px-8 py-4 flex gap-4 flex-wrap">
+                <Cards />
+            </div>
+        </>
+    )
+}
