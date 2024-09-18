5406b774cd56ceea80750c4336f0418aedd7b8ae
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2024-09-18 12:19:30 +0530

    to add products we set one form to add product now we are going to remove the api to display changes on web

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import Details from './components/Details.jsx'
 import Home from './components/Home.jsx';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useLocation } from 'react-router-dom';
 import { FaHome } from "react-icons/fa";
+import Create from './components/Create.jsx';
 
 export default function App(){
+  const {search, pathname} = useLocation();
+  
   return (
     <div className="h-screen w-full flex">
-      <Link to="/" className="p-2 text-2xl bg-red-400 text-zinc-100 absolute right-0 rounded-full"><FaHome /></Link>
+      {
+        (pathname != '/' || search.length > 0) && (<Link to="/" className="p-1 text-lg rounded bg-blue-600 text-zinc-100 absolute right-1 top-1"><FaHome /></Link>)
+      }
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
         <Route path="/item-details/:id" element={<Details />} />
       </Routes>
     </div>
